0204f85b92d77acb094310475d08f96f063eeef6
eddieX-2021 <eddiexiao2019@gmail.com>
2025-07-31 16:04:48 -0400

    apply gitignore

--- next-app/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Gs4RU4abOJZsbZQkGOUVzy44rMkCZg8ujNtcV0JZrdE=\"\n}"
--- next-app/.next/static/chunks/app_[stock]_page_tsx_b2f3bef0._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_807f598b._.js"
-],
-    source: "dynamic"
-});
